44ba42dcff14727b9e6c275b61cf730830f21c3e
Edvard LUNDIN <edvard.lundin@pm.me>
2025-11-20 19:52:54 +0100

    chore: redeployed cluster
    Signed-off-by: Edvard LUNDIN <edvard.lundin@pm.me>

--- terraform/main.tf
+++ terraform/main.tf
@@ -92,25 +92,25 @@ locals {
       ipv4_address     = "192.168.2.140/24"
       gateway_ipv4     = local.network_gateway
     },
-    "tailscale-exit-node" = {
-      node_name        = var.proxmox_node_name
-      vm_id            = 143
-      start_at_boot    = true
-      started          = true
-      os_template_path = local.debian_13_lxc_template_path
-      os_type          = "debian"
-      unprivileged     = true
-      nesting          = true
-      keyctl           = false
-      description      = "Tailscale exit node into ProtonVPN"
-      cores            = 2
-      memory           = 1024
-      swap             = local.swap_size
-      disk_size        = 4
-      dns_server       = local.dns_server
-      ipv4_address     = "192.168.2.143/24"
-      gateway_ipv4     = local.network_gateway
-    },
+    # "tailscale-exit-node" = {
+    #   node_name        = var.proxmox_node_name
+    #   vm_id            = 143
+    #   start_at_boot    = true
+    #   started          = true
+    #   os_template_path = local.debian_13_lxc_template_path
+    #   os_type          = "debian"
+    #   unprivileged     = true
+    #   nesting          = true
+    #   keyctl           = false
+    #   description      = "Tailscale exit node into ProtonVPN"
+    #   cores            = 2
+    #   memory           = 1024
+    #   swap             = local.swap_size
+    #   disk_size        = 4
+    #   dns_server       = local.dns_server
+    #   ipv4_address     = "192.168.2.143/24"
+    #   gateway_ipv4     = local.network_gateway
+    # },
     # "wirebos" = {
     #   node_name        = var.proxmox_node_name
     #   vm_id            = 200
@@ -512,7 +512,7 @@ resource "null_resource" "install_sealed_secrets" {
       "cat <<EOF | kubectl apply -f -",
       "${data.local_file.sealed-secret-keys.content}",
       "EOF",
-      "kubectl apply -f https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.32.2/controller.yaml",
+      "kubectl apply -f https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.33.1/controller.yaml",
     ]
   }
 }
